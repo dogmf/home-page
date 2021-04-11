3954e5a8658bc81fa7b6049ab309d3b9156e44bc
dogmf <dogmf@mail.ru>
2021-04-11 15:19:52 +0300

    added mobile motion capture - Added and tested capturing on IOS and chrome dev tools - Moved source files for Photo component to /src folder

--- src/MyPhoto/MyPhoto.jsx
+++ src/MyPhoto/MyPhoto.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
-import FaceImage from "./dogmf_face_face.png";
-import GlassesImage from "./dogmf_face_glasses.png";
-import ShadowImage from "./dogmf_face_shadow.png";
-import HairImage from "./dogmf_face_hair.png";
-import CLASSES from "./myPhoto.module.css";
+import React, { useCallback, useEffect, useState } from "react";
+import FaceImage from "./src/dogmf_face_face.png";
+import GlassesImage from "./src/dogmf_face_glasses.png";
+import ShadowImage from "./src/dogmf_face_shadow.png";
+import HairImage from "./src/dogmf_face_hair.png";
+import CLASSES from "./src/myPhoto.module.css";
+import subscribeToMotion from "./subscribeToMotion";
+import useDeviceOrientationGrant from "./useDeviceOrientationGrant";
 
 const imageMoveStyle = (
   [x = 0, y = 0] = [0, 0],
@@ -11,46 +13,19 @@ const imageMoveStyle = (
   type = "translate"
 ) => ({ position: "absolute", transform: `translate(${x * k}px, ${y * k}px)` });
 
-const getSize = () => {
-  var body = document.body,
-    html = document.documentElement;
-
-  var height = Math.max(
-    body.scrollHeight,
-    body.offsetHeight,
-    html.clientHeight,
-    html.scrollHeight,
-    html.offsetHeight
-  );
-  var width = Math.max(
-    body.scrollWidth,
-    body.offsetWidth,
-    html.clientWidth,
-    html.scrollWidth,
-    html.offsetWidth
-  );
-  return [width, height];
-};
-
-const getP = (s, p) => (p - s / 2) / (s / 2);
-const getMoveHandler = (callback) => {
-  return (e) => {
-    let [w, h] = getSize();
-    callback([getP(w, e.x), getP(h, e.y)]);
-  };
-};
-
 function MyPhoto() {
   const [coords, setCoords] = useState([0, 0]);
+  const { granted, getGrant } = useDeviceOrientationGrant();
 
   useEffect(() => {
-    let moveHandler = getMoveHandler(setCoords);
-    window.addEventListener("mousemove", moveHandler);
-    return () => window.removeEventListener("mousemove", moveHandler);
+    return subscribeToMotion(setCoords);
   }, []);
 
   return (
-    <div className={CLASSES.containerBlock}>
+    <div
+      onClick={granted === false && getGrant}
+      className={CLASSES.containerBlock}
+    >
       <img
         className={CLASSES.image}
         src={FaceImage}
---
+++ src/MyPhoto/subscribeToMotion.js
@@ -0,0 +1,77 @@
+const getSize = () => {
+  var body = document.body,
+    html = document.documentElement;
+
+  var height = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  );
+  var width = Math.max(
+    body.scrollWidth,
+    body.offsetWidth,
+    html.clientWidth,
+    html.scrollWidth,
+    html.offsetWidth
+  );
+  return [width, height];
+};
+
+const getP = (size, point) => (point - size / 2) / (size / 2);
+
+const setMouseMoveListener = (callback) => {
+  const handler = (e) => {
+    let [w, h] = getSize();
+    callback([getP(w, e.x), getP(h, e.y)]);
+  };
+  window.addEventListener("mousemove", handler);
+  const removeListener = () => window.removeEventListener("mousemove", handler);
+  return removeListener;
+};
+const DEFAULT_RATIO = 30;
+
+const getCenter = (point, center = point, ratio) => {
+  let difference = center - point;
+  if (Math.abs(difference) < ratio) return center;
+  return point + Math.sign(difference) * ratio;
+};
+const modifyPoint = (point, center, ratio) => {
+  return (point - center) / ratio;
+};
+const setDeviceMoveListener = (callback) => {
+  let xCenter;
+  let yCenter;
+  const handler = (e) => {
+    console.log(e);
+    let x = e.beta;
+    let y = e.gamma;
+    xCenter = getCenter(x, xCenter, DEFAULT_RATIO);
+    yCenter = getCenter(y, yCenter, DEFAULT_RATIO);
+    let mx = modifyPoint(x, xCenter, DEFAULT_RATIO);
+    let my = modifyPoint(y, yCenter, DEFAULT_RATIO);
+    console.log({
+      x,
+      y,
+      xCenter,
+      yCenter,
+      coords: [my, mx],
+    });
+    callback([my, mx]);
+  };
+  window.addEventListener("deviceorientation", handler);
+  const removeListener = () =>
+    window.removeEventListener("deviceorientation", handler);
+  return removeListener;
+};
+const subscribeToMotion = (callback) => {
+  let cb = (coords) => callback(coords.map((n) => n.toFixed(2)));
+  const removeDeviceMoveListener = setDeviceMoveListener(cb);
+  const removeMouseMoveListener = setMouseMoveListener(cb);
+  return () => {
+    removeMouseMoveListener();
+    removeDeviceMoveListener();
+  };
+};
+export default subscribeToMotion;
